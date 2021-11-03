e1ffb9efbe6d3517fe9774a5ca5eea9f97b9cb4d
franarci <fran.arciniega96@gmail.com>
2021-11-03 20:50:44 -0300

    Agregar archivos de proyecto.

---
+++ WindowsPubs/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsPubs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostrarTiendas = new System.Windows.Forms.Button();
+            this.gridPubs = new System.Windows.Forms.DataGridView();
+            this.btnTraerAutorPorId = new System.Windows.Forms.Button();
+            this.btnTraerAutoresPorCiudad = new System.Windows.Forms.Button();
+            this.btnTraerTodosPubs = new System.Windows.Forms.Button();
+            this.btnTraerTodosAutores = new System.Windows.Forms.Button();
+            this.btnTraerPubPorNombre = new System.Windows.Forms.Button();
+            this.btnMostrarPublicadoresYEmpleados = new System.Windows.Forms.Button();
+            this.btnFiltrarEmpleadosPublicadores = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridPubs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnMostrarTiendas
+            // 
+            this.btnMostrarTiendas.Location = new System.Drawing.Point(54, 231);
+            this.btnMostrarTiendas.Name = "btnMostrarTiendas";
+            this.btnMostrarTiendas.Size = new System.Drawing.Size(163, 23);
+            this.btnMostrarTiendas.TabIndex = 11;
+            this.btnMostrarTiendas.Text = "Mostrar Tiendas y Ventas";
+            this.btnMostrarTiendas.UseVisualStyleBackColor = true;
+            this.btnMostrarTiendas.Click += new System.EventHandler(this.btnMostrarTiendas_Click);
+            // 
+            // gridPubs
+            // 
+            this.gridPubs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridPubs.Location = new System.Drawing.Point(282, 62);
+            this.gridPubs.Name = "gridPubs";
+            this.gridPubs.Size = new System.Drawing.Size(879, 344);
+            this.gridPubs.TabIndex = 10;
+            // 
+            // btnTraerAutorPorId
+            // 
+            this.btnTraerAutorPorId.Location = new System.Drawing.Point(54, 173);
+            this.btnTraerAutorPorId.Name = "btnTraerAutorPorId";
+            this.btnTraerAutorPorId.Size = new System.Drawing.Size(163, 23);
+            this.btnTraerAutorPorId.TabIndex = 9;
+            this.btnTraerAutorPorId.Text = "Traer autor por id";
+            this.btnTraerAutorPorId.UseVisualStyleBackColor = true;
+            this.btnTraerAutorPorId.Click += new System.EventHandler(this.btnTraerAutorPorId_Click);
+            // 
+            // btnTraerAutoresPorCiudad
+            // 
+            this.btnTraerAutoresPorCiudad.Location = new System.Drawing.Point(54, 144);
+            this.btnTraerAutoresPorCiudad.Name = "btnTraerAutoresPorCiudad";
+            this.btnTraerAutoresPorCiudad.Size = new System.Drawing.Size(163, 23);
+            this.btnTraerAutoresPorCiudad.TabIndex = 8;
+            this.btnTraerAutoresPorCiudad.Text = "Traer autores por ciudad";
+            this.btnTraerAutoresPorCiudad.UseVisualStyleBackColor = true;
+            this.btnTraerAutoresPorCiudad.Click += new System.EventHandler(this.btnTraerAutoresPorCiudad_Click);
+            // 
+            // btnTraerTodosPubs
+            // 
+            this.btnTraerTodosPubs.Location = new System.Drawing.Point(54, 115);
+            this.btnTraerTodosPubs.Name = "btnTraerTodosPubs";
+            this.btnTraerTodosPubs.Size = new System.Drawing.Size(163, 23);
+            this.btnTraerTodosPubs.TabIndex = 7;
+            this.btnTraerTodosPubs.Text = "Traer todos los publicadores";
+            this.btnTraerTodosPubs.UseVisualStyleBackColor = true;
+            this.btnTraerTodosPubs.Click += new System.EventHandler(this.btnTraerTodosPubs_Click);
+            // 
+            // btnTraerTodosAutores
+            // 
+            this.btnTraerTodosAutores.Location = new System.Drawing.Point(54, 86);
+            this.btnTraerTodosAutores.Name = "btnTraerTodosAutores";
+            this.btnTraerTodosAutores.Size = new System.Drawing.Size(163, 23);
+            this.btnTraerTodosAutores.TabIndex = 6;
+            this.btnTraerTodosAutores.Text = "Traer todos los autores";
+            this.btnTraerTodosAutores.UseVisualStyleBackColor = true;
+            this.btnTraerTodosAutores.Click += new System.EventHandler(this.btnTraerTodosAutores_Click);
+            // 
+            // btnTraerPubPorNombre
+            // 
+            this.btnTraerPubPorNombre.Location = new System.Drawing.Point(54, 202);
+            this.btnTraerPubPorNombre.Name = "btnTraerPubPorNombre";
+            this.btnTraerPubPorNombre.Size = new System.Drawing.Size(163, 23);
+            this.btnTraerPubPorNombre.TabIndex = 12;
+            this.btnTraerPubPorNombre.Text = "Traer publicador por nombre";
+            this.btnTraerPubPorNombre.UseVisualStyleBackColor = true;
+            this.btnTraerPubPorNombre.Click += new System.EventHandler(this.btnTraerPubPorNombre_Click);
+            // 
+            // btnMostrarPublicadoresYEmpleados
+            // 
+            this.btnMostrarPublicadoresYEmpleados.Location = new System.Drawing.Point(29, 260);
+            this.btnMostrarPublicadoresYEmpleados.Name = "btnMostrarPublicadoresYEmpleados";
+            this.btnMostrarPublicadoresYEmpleados.Size = new System.Drawing.Size(188, 23);
+            this.btnMostrarPublicadoresYEmpleados.TabIndex = 13;
+            this.btnMostrarPublicadoresYEmpleados.Text = "Mostrar empleados y publicadores";
+            this.btnMostrarPublicadoresYEmpleados.UseVisualStyleBackColor = true;
+            this.btnMostrarPublicadoresYEmpleados.Click += new System.EventHandler(this.btnMostrarPublicadoresYEmpleados_Click);
+            // 
+            // btnFiltrarEmpleadosPublicadores
+            // 
+            this.btnFiltrarEmpleadosPublicadores.Location = new System.Drawing.Point(29, 289);
+            this.btnFiltrarEmpleadosPublicadores.Name = "btnFiltrarEmpleadosPublicadores";
+            this.btnFiltrarEmpleadosPublicadores.Size = new System.Drawing.Size(188, 23);
+            this.btnFiltrarEmpleadosPublicadores.TabIndex = 14;
+            this.btnFiltrarEmpleadosPublicadores.Text = "Filtrar empleados y publicadores";
+            this.btnFiltrarEmpleadosPublicadores.UseVisualStyleBackColor = true;
+            this.btnFiltrarEmpleadosPublicadores.Click += new System.EventHandler(this.btnFiltrarEmpleadosPublicadores_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1221, 450);
+            this.Controls.Add(this.btnFiltrarEmpleadosPublicadores);
+            this.Controls.Add(this.btnMostrarPublicadoresYEmpleados);
+            this.Controls.Add(this.btnTraerPubPorNombre);
+            this.Controls.Add(this.btnMostrarTiendas);
+            this.Controls.Add(this.gridPubs);
+            this.Controls.Add(this.btnTraerAutorPorId);
+            this.Controls.Add(this.btnTraerAutoresPorCiudad);
+            this.Controls.Add(this.btnTraerTodosPubs);
+            this.Controls.Add(this.btnTraerTodosAutores);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridPubs)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMostrarTiendas;
+        private System.Windows.Forms.DataGridView gridPubs;
+        private System.Windows.Forms.Button btnTraerAutorPorId;
+        private System.Windows.Forms.Button btnTraerAutoresPorCiudad;
+        private System.Windows.Forms.Button btnTraerTodosPubs;
+        private System.Windows.Forms.Button btnTraerTodosAutores;
+        private System.Windows.Forms.Button btnTraerPubPorNombre;
+        private System.Windows.Forms.Button btnMostrarPublicadoresYEmpleados;
+        private System.Windows.Forms.Button btnFiltrarEmpleadosPublicadores;
+    }
+}
+
---
+++ WindowsPubs/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsPubs.Data;
+
+namespace WindowsPubs
+{
+    public partial class Form1 : Form
+    {
+        PubsContext context = new PubsContext();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTraerTodosAutores_Click(object sender, EventArgs e)
+        {
+            var lista = (from a in context.authors
+                         select a).ToList();
+
+            gridPubs.DataSource = lista;
+        }
+
+        private void btnTraerTodosPubs_Click(object sender, EventArgs e)
+        {
+            var lista = (from a in context.publishers
+                         select a).ToList();
+
+            gridPubs.DataSource = lista;
+        }
+
+        private void btnTraerAutoresPorCiudad_Click(object sender, EventArgs e)
+        {
+            string ciudad = "Oakland";
+            var lista = (from a in context.authors
+                         where a.city == ciudad
+                         select a).ToList();
+            gridPubs.DataSource = lista;
+        }
+
+        private void btnTraerAutorPorId_Click(object sender, EventArgs e)
+        {
+            string id = "807-91-6654";
+            var autor = (from a in context.authors
+                            where a.au_id == id
+                            select a).SingleOrDefault();
+            MessageBox.Show(autor.au_lname + " "+ autor.au_fname);
+        }
+
+        private void btnTraerPubPorNombre_Click(object sender, EventArgs e)
+        {
+            string nombre = "Ramona Publishers";
+            var publicador = (from p in context.publishers
+                         where p.pub_name == nombre
+                         select p).SingleOrDefault();
+            MessageBox.Show(publicador.pub_id+" "+publicador.pub_name);
+        }
+
+        private void btnMostrarTiendas_Click(object sender, EventArgs e)
+        {
+            var lista = (
+                        from sa in context.sales
+                        join st in context.stores
+                        on sa.stor_id equals st.stor_id
+                        select new
+                        {
+                            Id = st.stor_id,
+                            NombreTienda = st.stor_name,
+                            Ciudad = st.city,
+                            NroOrden=sa.ord_num,
+                            Fecha=sa.ord_date,
+                            Cantidad = sa.qty,
+                            IdTitulo = sa.title_id
+                        }
+                ).ToList();
+            gridPubs.DataSource = lista;
+        }
+
+        private void btnMostrarPublicadoresYEmpleados_Click(object sender, EventArgs ev)
+        {
+            var lista = (
+                        from e in context.employees
+                        join p in context.publishers
+                        on e.pub_id equals p.pub_id
+                        select new
+                        {
+                            
+                            Nombre = e.fname,
+                            Apellido = e.lname,
+                            NombrePublicador = p.pub_name,
+                        }
+                ).ToList();
+            gridPubs.DataSource = lista;
+        }
+
+        private void btnFiltrarEmpleadosPublicadores_Click(object sender, EventArgs ev)
+        {
+            string pais = "USA";
+            var lista = (
+                        from e in context.employees
+                        join p in context.publishers
+                        on e.pub_id equals p.pub_id
+                        where p.country == pais
+                        select new
+                        {
+
+                            Nombre = e.fname,
+                            Apellido = e.lname,
+                            NombrePublicador = p.pub_name,
+                        }
+
+                ).ToList();
+            gridPubs.DataSource = lista;
+        }
+    }
+}
